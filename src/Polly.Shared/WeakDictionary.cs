1604110b64f655a86e4bab27154465bd8ff61cb0
IEUser <IEUser@MSEDGEWIN10>
2016-08-29 10:25:24 -0400

    WIP

---
+++ src/Polly.Shared/WeakDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace Polly.Shared
+{
+    public class WeakDictionary<K, V> where V : class
+    {
+        private HashSet<KeyValuePair<K, WeakReference<V>>> list;
+
+        public WeakDictionary()
+        {
+            list = new HashSet<KeyValuePair<K, WeakReference<V>>>();
+        }
+
+        public void Add(K k, V v)
+        {
+            list.Add(new KeyValuePair<K, WeakReference<V>>(k, new WeakReference<V>(v)));
+        }
+
+        public IEnumerable<KeyValuePair<K, V>> All()
+        {
+            var cloneList = new HashSet<KeyValuePair<K, WeakReference<V>>>(list);
+            foreach (var pair in cloneList)
+            {
+                V value;
+                if (pair.Value.TryGetTarget(out value))
+                {
+                    yield return new KeyValuePair<K,V>(pair.Key, value);
+                //} else
+                //{
+                //    list.Remove(pair);
+                }
+            }
+        }
+    }
+}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
---
+++ src/Polly.SharedSpecs/WeakDictionarySpecs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Polly.Specs
+{
+    public class WeakDictionarySpecs
+    {
+        [Fact]
+        public void Enumerable()
+        {
+            var dict = new Shared.WeakDictionary<string, object>();
+            var fred = new object();
+            dict.Add("fred", fred);
+
+            var count = 0;
+            foreach(var entry in dict.All())
+            {
+                Assert.Equal(entry.Key, "fred");
+                count++;
+            }
+            Assert.Equal(count, 1);
+        }
+
+        [Fact]
+        public void DoesNotHaveAStrongReference()
+        {
+            var dict = new Shared.WeakDictionary<string, object>();
+            var fred = new object();
+            dict.Add("fred", fred);
+
+            Assert.Equal(dict.All().Count(), 1);
+
+            fred = null;
+            GC.Collect();
+
+            var list = dict.All().ToArray();
+            Assert.Equal(list.Count(), 0);
+        }
+
+        [Fact]
+        public void CanReAddKeys()
+        {
+            var dict = new Shared.WeakDictionary<string, object>();
+            var fred = new object();
+            dict.Add("fred", fred);
+            Assert.Equal(dict.All().Count(), 1);
+
+            fred = null;
+            GC.Collect();
+            Assert.Equal(dict.All().Count(), 0);
+
+            fred = new object();
+            dict.Add("fred", fred);
+            Assert.Equal(dict.All().Count(), 1);
+        }
+    }
+}
